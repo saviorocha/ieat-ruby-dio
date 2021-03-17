44a0a6a17d4c59001b7e525e992821160d33b6b8
saviorocha <44814299+saviorocha@users.noreply.github.com>
2021-03-16 23:01:19 -0300

    Aula de Job e Workers

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,20 @@
+class OrderController < ApplicationController
+    def create
+        @order = Order.new(order_params)
+        @order.user_id = current_user.id
+        @order.status = :pending
+        
+        if @order.save
+            flash[:notice] = 'Pedido criado com sucesso'
+            RestaurantNotifierJob.perform_later(current_user.id)
+        else
+            flash[:error] = 'Falha ao criar pedido'        
+        end
+        redirect_to root_path    
+    end
+    private 
+
+    def order_params
+        params.permit(:product_id)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :restaurant
+  has_many :orders
   enum role: [:admin, :restaurant, :customer]
 end
